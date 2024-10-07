4a8a7d0ae7c6970976f953f5e903329f51230cbf
Ahmad Mamoon <ahmadeveloper077@gmail.com>
2024-10-08 00:15:06 +0500

    Update changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,8 @@ module.exports = {
   darkMode: ['class'],
   content: [
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}', // lowercase "components"
+    './Component/**/*.{js,ts,jsx,tsx,mdx}', // capitalized "Component"
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
